9390a9dcf82dbae7377f765867e1e6b013e87b71
Oscar <oscaar_2003@hotmail.com>
2022-05-01 23:55:22 -0300

    adding repositories

---
+++ NorthWind/NorthWind.Repositories.EFCore/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,21 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.POCOEntities;
+using NorthWind.Repositories.EFCore.DataContext;
+
+namespace NorthWind.Repositories.EFCore.Repositories
+{
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        readonly NorthWindContext context;
+
+        public OrderDetailRepository(NorthWindContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(OrderDetail orderDetail)
+        {
+            context.Add(orderDetail);
+        }
+    }
+}
