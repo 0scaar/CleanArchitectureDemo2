2815eee093d5d03f57a4a4c2c2f04c27dd23654f
Oscar <oscaar_2003@hotmail.com>
2022-05-02 09:19:50 -0300

    adding WebApi project

---
+++ NorthWind/NorthWind.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NorthWind.UseCases.CreateOrder;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        readonly IMediator mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateOrder(CreateOrderInputPort orderParams)
+        {
+            return await mediator.Send(orderParams);
+        }
+    }
+}
---
+++ NorthWind/NorthWind.WebApi/Startup.cs
@@ -0,0 +1,66 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using NorthWind.Entities.Exceptions;
+using NorthWind.IoC;
+using NorthWind.WebExceptionsPresenter;
+using System;
+using System.Collections.Generic;
+
+namespace NorthWind.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers(option => option.Filters.Add(new ApiExceptionFilterAttribute(
+                new Dictionary<Type, IExceptionHandler> 
+                {
+                    { typeof(GeneralException), new GeneralExceptionHandler() },
+                    { typeof(ValidationException), new ValidationExceptionHandler() }
+                })));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NorthWind.WebApi", Version = "v1" });
+            });
+
+            services.AddNorthWindService(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NorthWind.WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
