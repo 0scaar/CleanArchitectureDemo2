1669d425925bf319f42f4267edbab5dd569d1c20
Oscar <oscaar_2003@hotmail.com>
2022-05-02 00:47:12 -0300

    adding DTOs and useCase project. technology mediatR, fluentValidator

---
+++ NorthWind/NorthWind.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using NorthWind.Entities.Exceptions;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.POCOEntities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.CreateOrder
+{
+    public class CreateOrderInteractor : IRequestHandler<CreateOrderInputPort, int>
+    {
+        readonly IOrderRepository orderRepository;
+        readonly IOrderDetailRepository orderDetailRepository;
+        readonly IUnitOfWork unitOfWork;
+
+        public CreateOrderInteractor(IOrderRepository orderRepository, IOrderDetailRepository orderDetailRepository, IUnitOfWork unitOfWork)
+        {
+            this.orderRepository = orderRepository;
+            this.orderDetailRepository = orderDetailRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Handle(CreateOrderInputPort request, CancellationToken cancellationToken)
+        {
+            var order = new Order
+            {
+                CustomerId = request.CustomerId,
+                OrderDate = DateTime.Now,
+                ShipAddress = request.ShipAddress,
+                ShipCity = request.ShipCity,
+                ShipCountry = request.ShipCountry,
+                ShipPostalCode = request.ShipPostalCode,
+                shippingType = Entities.Enums.ShippingType.Road,
+                DiscountType = Entities.Enums.DiscountType.Percentage,
+                Discount = 10
+            };
+
+            orderRepository.Create(order);
+
+            foreach(var item in request.OrderDetails)
+            {
+                orderDetailRepository.Create(new OrderDetail
+                {
+                    Order = order,
+                    ProductId = item.ProductId,
+                    UnitPrice = item.UnitPrice,
+                    Quantity = item.Quantity
+                });
+            }
+
+            try
+            {
+                await unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new GeneralException("Error creating order", ex.Message);
+            }
+
+            return order.Id;
+        }
+    }
+}
---
+++ NorthWind/NorthWind.UseCases/CreateOrder/CreateOrderValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System.Linq;
+
+namespace NorthWind.UseCases.CreateOrder
+{
+    public class CreateOrderValidator : AbstractValidator<CreateOrderInputPort>
+    {
+        public CreateOrderValidator()
+        {
+            RuleFor(c => c.CustomerId)
+                .NotEmpty()
+                .WithMessage("You must provide the client identifier");
+
+            RuleFor(c => c.ShipAddress)
+                .NotEmpty()
+                .WithMessage("You must provide the shipping address");
+
+            RuleFor(c => c.ShipCity)
+                .NotEmpty()
+                .MinimumLength(3)
+                .WithMessage("You must provide at least 3 characters of the city name");
+
+            RuleFor(c => c.ShipCountry)
+                .NotEmpty()
+                .MinimumLength(3)
+                .WithMessage("You must provide at least 3 characters of the country name");
+
+            RuleFor(c => c.OrderDetails)
+                .Must(d => d != null && d.Any())
+                .WithMessage("The products of the order must be specified");
+        }
+    }
+}
