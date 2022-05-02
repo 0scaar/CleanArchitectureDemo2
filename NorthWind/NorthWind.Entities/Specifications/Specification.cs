8c92edf7896ec3727e254e27a0abfc666670812f
Oscar <oscaar_2003@hotmail.com>
2022-05-01 23:20:54 -0300

    adding migrations and contracts

---
+++ NorthWind/NorthWind.Entities/Specifications/Specification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace NorthWind.Entities.Specifications
+{
+    public abstract class Specification<T>
+    {
+        public Expression<Func<T, bool>> Expression { get; set; }
+
+        public bool IsSatisfieldBy(T entity)
+        {
+            Func<T, bool> expressionDelegate = Expression.Compile();
+
+            return expressionDelegate(entity);
+        }
+    }
+}
